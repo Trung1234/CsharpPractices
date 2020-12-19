6a8fbecb4d4c653f4dba1a03d9196a7187b81527
unknown <trungsieunhan24@gmail.com>
2020-12-19 06:11:30 -0800

    add CoffeeShopManagement

---
+++ CoffeeShopManagement/CoffeeShopManager/LoginForm.Designer.cs
@@ -0,0 +1,86 @@
+﻿namespace CoffeeShopManager
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.panel2);
+            this.panel1.Location = new System.Drawing.Point(13, 13);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(499, 196);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.labelLogin);
+            this.panel2.Location = new System.Drawing.Point(4, 4);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(485, 58);
+            this.panel2.TabIndex = 0;
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.labelLogin.Location = new System.Drawing.Point(4, 4);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(64, 15);
+            this.labelLogin.TabIndex = 0;
+            this.labelLogin.Text = "Login name";
+            // 
+            // LoginForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 221);
+            this.Controls.Add(this.panel1);
+            this.Name = "LoginForm";
+            this.Text = "Login ";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label labelLogin;
+    }
+}
+
---
+++ CoffeeShopManagement/CoffeeShopManager/Scripts/Table.sql
@@ -0,0 +1,72 @@
+CREATE DATABASE CoffeShop
+GO
+USE CoffeShop
+GO
+
+
+-- Food
+-- Table
+-- FoodCategory
+-- Account
+-- Bill
+-- BillInfo
+
+CREATE TABLE TableFood
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Bàn ch?a có tên',
+	status NVARCHAR(100) NOT NULL DEFAULT N'Tr?ng'	-- Tr?ng || Có ng??i
+)
+
+GO
+
+CREATE TABLE Account
+(
+	UserName  NVARCHAR(100) PRIMARY KEY,
+	DisplayName NVARCHAR(100) NOT NULL DEFAULT N'DisplayName',
+	PassWord NVARCHAR(1000) NOT NULL DEFAULT 0,
+	Type INT NOT NULL  DEFAULT 0 -- 1: admin && 0: staff
+)
+
+GO
+
+CREATE TABLE FoodCategory
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Ch?a ??t tên'
+)
+GO
+
+CREATE TABLE Food
+(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL DEFAULT N'Ch?a ??t tên',
+	idCategory INT NOT NULL,
+	price FLOAT NOT NULL DEFAULT 0	
+	FOREIGN KEY (idCategory) REFERENCES FoodCategory(id)
+)
+GO
+
+CREATE TABLE Bill
+(
+	id INT IDENTITY PRIMARY KEY,
+	DateCheckIn DATE NOT NULL DEFAULT GETDATE(),
+	DateCheckOut DATE,
+	idTable INT NOT NULL,
+	status INT NOT NULL DEFAULT 0 -- 1: ?ã thanh toán && 0: ch?a thanh toán
+	
+	FOREIGN KEY (idTable) REFERENCES dbo.TableFood(id)
+)
+GO
+
+CREATE TABLE BillInfo
+(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idFood INT NOT NULL,
+	count INT NOT NULL DEFAULT 0
+	
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+	FOREIGN KEY (idFood) REFERENCES dbo.Food(id)
+)
+GO
