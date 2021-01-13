6d9812f32e9d0841b84bdd5c10968030c9c12b90
unknown <trungsieunhan24@gmail.com>
2021-01-13 06:37:53 -0800

    create Ha noi tower app

---
+++ DataStructurePractice/DataStructurePractice/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DataStructurePractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DataStructurePractice/HaNoiTowerApp/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace HaNoiTowerApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DoTower(3, 'A', 'B', 'C');
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// solve the tower of HaNoi puzzle
+        /// </summary>
+        /// <param name="N"></param>
+        /// <param name="from"></param>
+        /// <param name="inner"></param>
+        /// <param name="to"></param>
+        static void DoTower(int N, char from, char inner, char to)
+        {
+            if(N == 1)
+            {
+                Console.WriteLine("Disk 1 from "+from+" to "+to);
+            }
+            else
+            {
+                // move N-1 disk from A -> B
+                DoTower(N - 1, from, to, inner);
+                // move disk N from A -> C
+                Console.WriteLine("Disk "+N+" from " + from + " to " + to);
+                // move N-1 disk from B -> C
+                DoTower(N - 1,inner,from,to);
+            }
+        } 
+    }
+}
