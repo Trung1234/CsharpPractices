a4973b41305339b21bbe2e508579f99b45acb075
unknown <trungsieunhan24@gmail.com>
2021-04-01 06:24:06 -0700

    fix startup

--- ContosoUniversityApp/ContosoUniversityApp/Startup.cs
+++ ContosoUniversityApp/ContosoUniversityApp/Startup.cs
@@ -50,7 +50,7 @@ namespace ContosoUniversityApp
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for studention scenarios, see https://aka.ms/aspnetcore-hsts.
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
